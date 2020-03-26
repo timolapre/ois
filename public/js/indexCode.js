9a6fdd5bd2016963a00453b9dc75223224123b82
timolapre <timolapre1998@gmail.com>
2020-03-26 21:11:51 +0100

    improvements

--- public/js/indexCode.js
+++ public/js/indexCode.js
@@ -1,37 +1,25 @@
 function goToOptionsPage() {
     var checkboxes = document.getElementsByClassName("checkbox");
-    var count = 0;
     var companies = ""
     for (i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             companies += checkboxes[i].id + ",";
-            count++;
         }
     }
     companies = companies.substring(0, companies.length - 1);
-    if (count > 0 && count <= 4) {
-        location.href = 'data2?companies=' + companies;
-    } else {
-        alert("Select between 1 and 4 please");
-    }
+    location.href = 'data2?companies=' + companies;
 }
 
 function goToComparePage() {
     var checkboxes = document.getElementsByClassName("checkbox");
-    var count = 0;
     var options = ""
     for (i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             options += checkboxes[i].id + ",";
-            count++;
         }
     }
     options = options.substring(0, options.length - 1);
-    if (count > 0) {
-        location.href = 'data3' + window.location.search + '&options=General,Content,' + options;
-    } else {
-        alert("Select at least one please");
-    }
+    location.href = 'data3' + window.location.search + '&options=General,Content,' + options;
 }
 
 function getCompanies() {
@@ -74,11 +62,11 @@ function getColsm() {
     var count = getCompanies().length;
     var colsm1 = 4;
     var colsm = 4;
-    if(count == 1){
+    if (count == 1) {
         colsm1 = 6;
     }
-    else if (count > 2) { 
-        colsm1 = 3; 
+    else if (count > 2) {
+        colsm1 = 3;
     }
 
     if (count == 1) {
@@ -93,6 +81,79 @@ function getColsm() {
     return [colsm1, colsm];
 }
 
-function search(){
+function search() {
     console.log("zoeken naar niks want dit werkt nog niet joe");
+}
+
+function selectAllGeneral() {
+    setTimeout(function () {
+        var checked = document.getElementById("checkboxgeneralall").checked;
+        var generalOptions = document.getElementsByClassName("checkboxgeneral");
+        if (checked) {
+            for (i = 0; i < generalOptions.length; i++) {
+                generalOptions[i].checked = true;
+            }
+        } else {
+            for (i = 0; i < generalOptions.length; i++) {
+                generalOptions[i].checked = false;
+            }
+        }
+        checkIfOptionsSelected();
+    }, 1);
+}
+
+function selectAllContent() {
+    setTimeout(function () {
+
+        var checked = document.getElementById("checkboxcontentall").checked;
+        var contentOptions = document.getElementsByClassName("checkboxcontent");
+        if (checked) {
+            for (i = 0; i < contentOptions.length; i++) {
+                contentOptions[i].checked = true;
+            }
+        } else {
+            for (i = 0; i < contentOptions.length; i++) {
+                contentOptions[i].checked = false;
+            }
+        }
+        checkIfOptionsSelected();
+    }, 1);
+}
+
+function checkIfCompaniesSelected() {
+    var checkboxes = document.getElementsByClassName("checkbox");
+    var count = 0;
+    var companies = ""
+    for (i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            companies += checkboxes[i].id + ",";
+            count++;
+        }
+    }
+    companies = companies.substring(0, companies.length - 1);
+    var button = document.getElementById("continuebutton");
+    if (count > 0 && count <= 4) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+}
+
+function checkIfOptionsSelected() {
+    var checkboxes = document.getElementsByClassName("checkbox");
+    var count = 0;
+    var companies = ""
+    for (i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            companies += checkboxes[i].id + ",";
+            count++;
+        }
+    }
+    companies = companies.substring(0, companies.length - 1);
+    var button = document.getElementById("continuebutton");
+    if (count > 0) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
 }
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var companies = ["Facebook", "Google", "Instagram", "Linkedin", "Tiktok", "Snapchat"];
 var optionsGeneral = ["Languages", "Formats", "Quality"];
 var optionsContent = ["Profile", "Gender", "Name", "First name", "Last name", "Full name","E-mails"];
-var marginBottom = Math.ceil(optionsGeneral.length/2)*100 +"px";
+var marginBottom = Math.ceil(optionsGeneral.length/2)*100+80 +"px";
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
